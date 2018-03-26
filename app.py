fb5e2e781fade06c98496cdb6c6fedfa5322a92a
Marie Starck <marie.compsci14@gmail.com>
2018-03-26 16:22:40 -0700

    Add SQLAlchemy to project

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://usr:pass@localhost/sqlalchemy'
+db = SQLAlchemy(app)
+db.create_all()
+db.session.commit()
 
 
 @app.route("/")
