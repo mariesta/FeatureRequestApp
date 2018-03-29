392091245886ef2e30bdd1aa558c7b044b5599c2
Marie Starck <marie.compsci14@gmail.com>
2018-03-28 19:29:58 -0700

    Only keep local db link

--- config.py
+++ config.py
@@ -1,11 +1,9 @@
 # edit the URI below to add your RDS password and your AWS URL
-# The other elements are the same as used in the tutorial
-# format: (user):(password)@(db_identifier).amazonaws.com:3306/(db_name)
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://mariesta:lionce2u@featureapp.cro2zrpxfcnb.us-east-2.rds.amazonaws.com/featureapp_db'
+# SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://(user):(password)@(db_endpoint)/(db_name)'
 
-# Uncomment the line below if you want to work with a local DB
-# SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/test.db'
+# Comment the line below if you want to work with a RDS account. Keep it as is for a local DB.
+SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/test2.db'
 
 
 WTF_CSRF_ENABLED = True
